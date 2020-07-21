c5b6ad2433efef5ce9151ce9ed3ac71b6f4278fc
Walber Beltrame <walber.beltrame@gmail.com>
2020-07-21 14:06:34 -0300

    Update code

---
+++ main.jl
@@ -0,0 +1,19 @@
+function e(x)
+ termo = 1
+ soma = 1
+ n = 1
+ while termo > 0
+  termo = termo * (x/n)
+  soma = soma + termo
+  n = n + 1
+ end
+ return [soma n]
+end
+
+y = e(1)
+println(y[1])
+println(y[2])
+
+y = e(2)
+println(y[1])
+println(y[2])
